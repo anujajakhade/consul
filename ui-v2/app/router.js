5d3b54c2084d1295927e00caef189baac809f32d
kenia <keniavalladarez@gmail.com>
2020-05-21 15:50:06 -0400

    Create Tags & Meta tab

--- ui-v2/app/router.js
+++ ui-v2/app/router.js
@@ -49,11 +49,8 @@ export const routes = {
         addresses: {
           _options: { path: '/addresses' },
         },
-        tags: {
-          _options: { path: '/tags' },
-        },
-        metadata: {
-          _options: { path: '/metadata' },
+        meta: {
+          _options: { path: '/meta' },
         },
       },
       notfound: {
